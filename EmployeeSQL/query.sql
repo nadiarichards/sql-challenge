57610b8628a562eab8f50bf36099f7b20eb8c645
nadiarichards <73898387+nadiarichards@users.noreply.github.com>
2021-01-21 13:30:40 -0500

    updated sql file

--- EmployeeSQL/query.sql
+++ EmployeeSQL/query.sql
@@ -6,6 +6,11 @@ LEFT JOIN salaries ON
 employees.emp_no = salaries.emp_no
 
 -- 2. List first name, last name, and hire date for employees who were hired in 1986.
+
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE EXTRACT ('year' from hire_date) = 1986;
+
 -- 3.List the manager of each department with the following information: 
 --department number, department name, the manager's employee number, last name, first name.
 -- 4.List the department of each employee with the following information: 
@@ -16,4 +21,5 @@ employees.emp_no = salaries.emp_no
 --first name, and department name.
 -- 7. List all employees in the Sales and Development departments, including their employee number, 
 --last name, first name, and department name.
--- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+-- 8. In descending order, list the frequency count of employee last names, i.e., 
+--how many employees share each last name.
